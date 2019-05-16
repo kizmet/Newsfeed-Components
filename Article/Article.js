e43985fa204e07c765537a3d20bc64d854a747b8
Kizmet <bryantpatton@gmail.com>
2019-05-15 17:14:12 -0700

    button shift article

--- Article/Article.js
+++ Article/Article.js
@@ -4,18 +4,34 @@ class Article {
   constructor(article) {
     this.article = article;
     this.expandButton = article.querySelector('button.expandButton');
+    this.expandButton.textContent = "Expand Article";
+    var hide = document.createElement('button');
+    this.hide = hide;
+    this.hide.setAttribute('class', 'expandButton hide');
+    this.hide.textContent = "Hide Article";
+    // this.expandButton.append(hide);
+    this.article.insertBefore(hide,this.expandButton);
     // this.expandButton.textContent = "expand";
+    // this.hideButton = article.querySelector('')
     this.expandButton.addEventListener('click', () => this.expandArticle());
-    // this.closeButton = article.createElement('button')
-    
+    this.hide.addEventListener('click', () => this.expandArticle());
+    // this.closeButton = article.createElement('button');
   }
+
 
   expandArticle() {
     this.article.classList.toggle('article-open');
-    this.expandButton(':before');
+    this.expandButton.classList.toggle('hide');
+    this.hide.classList.toggle('hide');
   }
+  // addButton () {
+
+  // }
 }
 
 
 const articles = document.querySelectorAll('.article');
 articles.forEach(article => new Article(article));
+
+
+
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,6 +1,8 @@
 const toggleMenu = () => {
     menu.classList.toggle('menu--open');
     // Toggle the "menu--open" class on your menu refence. 
+    articlesDiv.classList.toggle('menu--shift');
+    // articleDiv.forEach(article => {article.classList.toggle('menu--shift-art')})
 }
 
 
@@ -11,6 +13,8 @@ const menu = document.querySelector(".menu");
 
 const menuButton = document.querySelector(".menu-button");
 // Using your menuButton reference, add a click handler that calls toggleMenu
+const articlesDiv = document.querySelector('.articles')
+const articleDiv = document.querySelectorAll('.article')
 
 
 // menuButton.addEventListener('click', toggleMenu());
